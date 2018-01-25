e777be632219eec2b51d2d3c33cabdff6a97100c
salsvolatile <35800250+salsvolatile@users.noreply.github.com>
2018-01-25 15:33:18 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -11,7 +11,7 @@ record = {}
 root = lxml.html.fromstring(html)
 names = root.cssselect("article class div class h3")
 for name in names:
-  #print name.text
+  print name.text.encode('ascii','ignore')
   print name.attrib['href']
   #store the link in the variable under the key 'link'
   record ['link'] = name.attrib['href']
